57911a09bfe63f6933a00913bb7f5c640730e3c1
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-07 09:57:11 +0530

    os module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// // console.log("Hello , SPIDEY this side big fan of IRON MAN !");
+// console.log("Hello , SPIDEY this side big fan of IRON MAN !");
 // const citiesname = require('./data/mydata');
 // console.log(citiesname);
 
@@ -6,3 +6,14 @@
 
 import chalk from 'chalk';
 console.log(chalk.blue('hello spideyyy  bsf of iron man'));
+
+// const chalk = require('chalk');
+// console.log(chalk.blue('spideyy alwaays loved iron man'));
+// this works only upto v-4.0 of chalk no in v-5.0
+
+const os = require('os');
+const userInfo = os.userInfo();
+const platform = os.platform();
+const architecture = os.architecture();
+const uptime = os.uptime();
+console.log(userInfo);
