8944a2053305a4fb180b5b666cba3cdef1e503b9
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-07 09:47:57 +0530

    external module usage

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// console.log("Hello , SPIDEY this side big fan of IRON MAN !");
-const citiesname = require('./data/mydata');
-console.log(citiesname);
+// // console.log("Hello , SPIDEY this side big fan of IRON MAN !");
+// const citiesname = require('./data/mydata');
+// console.log(citiesname);
 
 
 
-const chalk
+import chalk from 'chalk';
+console.log(chalk.blue('hello spideyyy  bsf of iron man'));
