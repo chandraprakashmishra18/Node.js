a406a217c69f2b1cd9cb942137fdbdcb781f3eb8
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-10 12:35:50 +0530

    server-2

--- app.js
+++ app.js
@@ -33,13 +33,30 @@
 // require('dotenv').config();
 // console.log(process.env.PORT);
 
+// const http = require('http');
+// require("dotenv").config();
+// const Port = process.env.PORT || 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   res.write("<h1>Welcome to Node.js!</h1>");
+//   res.write("<p>Your first HTTP server is running.</p>");
+//   res.end();
+// });
+
 const http = require('http');
-require("dotenv").config();
-const Port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Welcome to Node.js!</h1>");
-  res.write("<p>Your first HTTP server is running.</p>");
-  res.end();
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+        <h1 style='color:navy'>Hello from Node.js!</h1>
+        <p>Built with pure Node.js http module.</p>
+    `);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Route not found' }));
+  }
 });
+
+server.listen(3000, () => console.log('Running on :3000'));
