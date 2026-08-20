a9647ad3e37975cdd4bee6b731076e4f1818c580
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-08-21 00:56:58 +0530

    commit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-// // importing the express
-// const express = require('express');
+// importing the express
+const express = require('express');
 
-// // called the function by factory method
+// called the function by factory method
 
-// const app = express();
+const app = express();
+const port = 3000;
 
-// // importing the packages from the tour.js file 
-// const packages = require('./data.tour');
 
-// // using home route to get the welcome message
+// importing the packages from the tour.js file 
+// const packages = require('./data/tour');
+
+// using home route to get the welcome message
 // app.get('/',(req,res)=>{
 //     res.send("Hi there !!! Welcome 🙏")
 // });
 
-// // getting the particular destination according to the route
+// getting the particular destination according to the route
 // app.get("/packages",(req,res)=>{
 //     if(!des){
 //         res.json(packages);
@@ -38,29 +40,44 @@
 // app.listen(5000,()=>{
 //     console.log("Server is running on port 5000")
 // })
-const express = require("express");
+//  Q console. log should print req. object refr to official doc
 
-const app = express();
-const port = 3000;
+// app.get('/',(req,res)=>{
+//     res.send("Hello good morning🙏")
+// });
+// // for post recall
+// app.post("/submit",(req, res)=>{
+//     res.send("This is the post method")
+// });
 
-const packageJson = require("./data/tour");
+// app.listen(3000,()=>{
+//     console.log("sever is running")
+// })
 
-app.get("/packages", (req, res) => {
-    const destination = req.query.des;
+// 17-08 to get the particular destination]
+// app.get("/packages",(req,res)=>{
+//     const des = req.query.des;
+//     if(!des){
+//         res.json(packages);
+//     }
+//     const result = packages.filter(
+//         (item) => item.destination.toLowerCase() == des.toLowerCase()
+//     );
+//     res.json(result);
+// })
 
-    if (!destination) {
-        res.json(packageJson);
-        return;
-    }
 
-    const filteredPackages = packageJson.filter(
-        (pkg) =>
-            pkg.destination.toLowerCase() === destination.toLowerCase()
-    );
+// app.listen(3000,()=>{
+//     console.log("sever is running")
+// })
+// ===========================================20-08-2026==========================================
+const tourRouter = require('./router/tourRouter');
 
-    res.json(filteredPackages);
-});
+app.use('/packages', tourRouter);
+// here packages is the home route and tourRouter is the router which we have created in the router
+//  folder
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+}
+);
