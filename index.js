bf26ec534df3594cc88da9f531a6aeae4a4d2729
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-23 15:10:52 +0530

    mongo db

--- index.js
+++ index.js
@@ -79,6 +79,33 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 // // here packages is the home route and tourRouter is the router which we have created in the router
 // //  folder
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://prashantmishra44140_db_user:<db_password>@cluster0.5vz2k5q.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }
